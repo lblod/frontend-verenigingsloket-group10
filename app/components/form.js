080e03bb816c39678582785298f94ba151064e4e
Dieter Peirs <github@dieterpeirs.com>
2024-09-17 11:08:13 +0200

    expand form

---
+++ app/components/form.js
@@ -0,0 +1,10 @@
+import Component from '@glimmer/component';
+
+
+export default class Form extends Component {
+  cities = [
+    { id: "0", value: "foo" },
+    { id: "1", value: "bar" },
+    { id: "2", value: "baz" }
+  ];
+}
