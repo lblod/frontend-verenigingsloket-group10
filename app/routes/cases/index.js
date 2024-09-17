c252994c54bf9f228728984de1f33077a55b9107
Erika Pauwels <erika.pauwels@gmail.com>
2024-09-17 19:36:19 +0200

    Fetch cases from backend in cases.index route

--- app/routes/cases/index.js
+++ app/routes/cases/index.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class CasesIndexRoute extends Route {}
+export default class CasesIndexRoute extends Route {
+  @service store;
+
+  model() {
+    return this.store.query('case', {
+      include: 'event,identifier,submissions'
+    });
+  }
+}
