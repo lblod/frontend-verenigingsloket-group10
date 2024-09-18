63bfba8a24b5d07d5adc50f6d61cd66b3c8059ec
Dieter Peirs <github@dieterpeirs.com>
2024-09-18 11:07:46 +0200

    style overviews

--- app/routes/cases.js
+++ app/routes/cases.js
@@ -3,8 +3,13 @@ import { service } from '@ember/service';
 
 export default class CasesRoute extends Route {
   @service session;
+  @service store;
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
+  }
+
+  async model() {
+    return this.store.findAll('case');
   }
 }
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,8 +3,9 @@ import { service } from '@ember/service';
 
 export default class DashboardRoute extends Route {
   @service session;
+  @service store;
 
-  beforeModel(transition) {
-    this.session.requireAuthentication(transition, 'login');
+  async model() {
+    return this.store.findAll('case');
   }
 }
