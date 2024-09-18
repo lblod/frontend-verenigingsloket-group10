ed4560ca1ef2a1009f376c9e0298ef72e88a7439
Erika Pauwels <erika.pauwels@gmail.com>
2024-09-18 11:30:10 +0200

    Add locations to events on submission

--- app/models/location.js
+++ app/models/location.js
@@ -2,6 +2,7 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class LocationModel extends Model {
   @attr uri;
+  @attr('string') name;
 
   @belongsTo('event', { inverse: 'locations', async: true }) event;
 }
