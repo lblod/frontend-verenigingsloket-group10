6e35159c722f6c6c2ef09876e5071264614c1502
Dieter Peirs <github@dieterpeirs.com>
2024-09-18 11:46:50 +0200

    add mock data

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -5,7 +5,7 @@ export const productData = [
     "description": "Inname van het openbaar domein voor de organisatie van een sportwedstrijd met nationale en internationale uitstraling",
     "Startdatum dienstverlening": "",
     "Einddatum dienstverlening": "nvt",
-    "categorie": "Vergunning",
+    "categorie": "Toelating",
     "Thema(’s)": "Andere organisatie, Burger, Lokaal bestuur",
     "Meer thema’s": "geen match",
     "Laatst gewijzigd": "2023-04-17",
@@ -40,5 +40,41 @@ export const productData = [
     "Contactgegevens": "",
     "filter": "tools",
     "filterTheme": "burger"
+  },
+  {
+    "name": "Drankvergunning vaste drankgelegenheid",
+    "description": "Als uitbater van een vaste drankgelegenheid (bijvoorbeeld een café, restaurant of cafetaria) heb je een drankvergunning nodig voor het schenken van alcoholhoudende dranken voor consumptie ter plaatse. Het gaat daarbij om gegiste dranken (bijvoorbeeld bier, wijn en mousserende wijn) en sterke drank (bijvoorbeeld gin, cognac en cocktails). De vergunning is persoons- en plaatsgebonden en kan niet worden overgedragen.",
+    "link": "#!",
+    "categorie": "Toelating",
+  },
+  {
+    "name": "Toelating voor ambulante activiteit op private plaatsen langs de openbare weg en op commerciële parkings",
+    "description": "Bij de uitbating van een kraam op privédomein is er sprake van een ambulante activiteit als er op dat adres geen vestigingseenheid in de KBO is geregistreerd en als het kraam aan de openbare weg grenst of geplaatst wordt op een commerciële parking. Voor die ambulante activiteit op privédomein is een toelating van de gemeente nodig. In andere gevallen is er sprake van een geregistreerd verkooppunt en geen ambulante activiteit, waardoor er geen toelating van de gemeente nodig is. ",
+    "link": "#!",
+    "categorie": "Toelating",
+  },
+  {
+    "name": "Terrasvergunning - Inname openbaar domein",
+    "description": "Voor het plaatsen van een horecaterras op het openbaar domein, vraag je als uitbater een terrasvergunning aan bij de gemeente. Het betreft de plaatsing van losse tafels en stoelen, parasols… voor zover die elementen makkelijk weg te nemen zijn en constructief geen geheel vormen. ",
+    "link": "#!",
+    "categorie": "Toelating",
+  },
+  {
+    "name": "Toelating om bij een evenement af te wijken van het geluidsniveau ",
+    "description": "Organiseer je een evenement met elektronische versterkte muziek dan moet je de geluidsnormen naleven. Wil je de muziek luider spelen dan de opgelegde geluidsnormen, dan kan je hiervoor een afwijking van de geluidsnormen aanvragen bij het college van burgemeester en schepenen.",
+    "link": "#!",
+    "categorie": "Toelating",
+  },
+  {
+    "name": "Organisatie van een rommelmarkt, tweedehandsbeurs of collectieve garageverkoop",
+    "description": "Op een rommelmarkt, tweedehandsbeurs of collectieve garageverkoop verkopen particulieren tweedehandsgoederen. Nieuwe of geproduceerde producten mogen ze er niet verkopen.",
+    "link": "#!",
+    "categorie": "Toelating",
+  },
+  {
+    "name": "Permanente bewegwijzering van een onderneming ",
+    "description": "Ondernemingen kunnen een aanvraag indienen om langs de openbare weg permanente wegwijzers te plaatsen naar hun onderneming. De wegbeheerder beoordeelt de aanvraag. Voor gemeentewegen is de gemeente bevoegd, voor gewestwegen is het Agentschap Wegen en Verkeer bevoegd.",
+    "link": "#!",
+    "categorie": "Toelating",
   }
 ]
